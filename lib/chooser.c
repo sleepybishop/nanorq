7e44cdc8f16355ebce7279d9f219b17ddce9a8e6
joseph calderon <calderon.joe@gmail.com>
2023-02-14 11:29:30 -0800

    at small symbol counts, decoder might run into a case of symbol not easily untangled

--- lib/chooser.c
+++ lib/chooser.c
@@ -2,7 +2,7 @@
 
 u32 precode_matrix_choose(void *arg, pc *W, u32 V0, u32 Vrows, u32 Srows, u32 Vcols)
 {
-    u32 chosen = Vrows;
+    u32 chosen = Srows, r = Vcols + 1;
     for (u32 b = 1; b < 3; b++) {
         while (uv_size(W->NZT[b]) > 0) {
             chosen = uv_pop(W->NZT[b]);
@@ -10,6 +10,12 @@ u32 precode_matrix_choose(void *arg, pc *W, u32 V0, u32 Vrows, u32 Srows, u32 Vc
                 return uv_A(W->di, chosen);
         }
     }
-    assert(0);
+    for (u32 row = V0; row < Srows; row++) {
+        u32 nz = uv_A(W->nz, uv_A(W->d, row));
+        if (nz > 0 && nz < r) {
+            chosen = row;
+            r = nz;
+        }
+    }
     return chosen;
 }
