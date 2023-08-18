4ecdfb23cfe0010d46ddd19a931af7ae662c83be
joseph calderon <calderon.joe@gmail.com>
2023-08-18 16:28:25 -0700

    Revert "at small symbol counts, decoder might run into a case of symbol not easily untangled"
    This reverts commit 7e44cdc8f16355ebce7279d9f219b17ddce9a8e6.

--- lib/chooser.c
+++ lib/chooser.c
@@ -2,7 +2,7 @@
 
 u32 precode_matrix_choose(void *arg, pc *W, u32 V0, u32 Vrows, u32 Srows, u32 Vcols)
 {
-    u32 chosen = Srows, r = Vcols + 1;
+    u32 chosen = Vrows;
     for (u32 b = 1; b < 3; b++) {
         while (uv_size(W->NZT[b]) > 0) {
             chosen = uv_pop(W->NZT[b]);
@@ -10,12 +10,6 @@ u32 precode_matrix_choose(void *arg, pc *W, u32 V0, u32 Vrows, u32 Srows, u32 Vc
                 return uv_A(W->di, chosen);
         }
     }
-    for (u32 row = V0; row < Srows; row++) {
-        u32 nz = uv_A(W->nz, uv_A(W->d, row));
-        if (nz > 0 && nz < r) {
-            chosen = row;
-            r = nz;
-        }
-    }
+    assert(0);
     return chosen;
 }
