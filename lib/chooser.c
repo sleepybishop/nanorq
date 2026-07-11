a5de98da5a87d7b9351b73f62ba815a9339a2a1f
joseph calderon <calderon.joe@gmail.com>
2026-07-11 13:41:26 -0700

    void unused parameters in precode_matrix_choose

--- lib/chooser.c
+++ lib/chooser.c
@@ -6,6 +6,8 @@
  */
 u32 precode_matrix_choose(void *arg, pc *W, u32 V0, u32 Vrows, u32 Srows,
                           u32 Vcols) {
+  (void)arg;
+  (void)Vcols;
   u32 chosen = Vrows;
   for (u32 b = 1; b < 3; b++) {
     while (uv_size(W->NZT[b]) > 0) {
